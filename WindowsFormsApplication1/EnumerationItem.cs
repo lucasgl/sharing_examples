3bbdf38128d2383a07e0ac3fd550baa6420bf128
katrinamsnyder <katrina.m1.snyder@gmail.com>
2015-04-07 16:48:35 -0400

    enum stuff

---
+++ WindowsFormsApplication1/EnumerationItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    public class EnumerationItem : BaseNotify
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                this.OnPropertyChanged("Name");
+            }
+        }
+
+        private int _value;
+
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+                this.OnPropertyChanged("Value");
+            }
+        }
+
+    }
+}
